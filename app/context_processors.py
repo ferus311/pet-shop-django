e1b26ef2311d371bfef56e93682a80ed48e615bc
Phong <fongcypher@gmail.com>
2024-08-19 15:06:55 +0700

    checkout view

--- app/context_processors.py
+++ app/context_processors.py
@@ -2,14 +2,25 @@ from django.db.models import Count
 from .models import (
     Category,
     Species,
+    CartDetail
 )
 
 
 def global_context(request):
-    return {
+    context = {
         "categories": Category.objects.annotate(
             num_products=Count("product")).order_by(
             "-num_products",
             "name"),
         "species": Species.objects.all(),
     }
+
+    if request.user.is_authenticated:
+        user = request.user
+        context["user"] = user
+        context["num_cart_items"] = CartDetail.objects.filter(
+            cart__user=request.user).count()
+    else:
+        context["num_cart_items"] = 0
+
+    return context
